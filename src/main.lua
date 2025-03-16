f3f06dae2afd7f2463699551577ed70941cd10b4
KauanLuc <kauan.lucena@sptech.school>
2025-03-16 03:24:54 -0300

    feat: the basic uses of the first commands in the help command

--- src/main.lua
+++ src/main.lua
@@ -129,7 +129,14 @@ function addf(...)
 end
 
 function help(...)
-	print('help*')
+	print(
+	[[
+	bruce init -> 
+	Initalize the Bruce Vault.
+	bruce add [--print, -p || --open-editor, -op] <credential || folder/credential> <gpg_id> -> 
+	Add a credential into Bruce Vault.
+	]]	
+	)
 end
 
 commands = {
